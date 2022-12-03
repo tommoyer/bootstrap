199fe9b1b9fbaeff71609bfcf90e1f7dabeb258f
Tom Moyer <tom.moyer@uncc.edu>
2022-12-02 19:58:33 -0500

    Tweaked bootstrap to be more modular

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+GNOME=0
+KDE=0
+SYSTEM76=0
+
+usage() {
+  echo "Usage: $0 [ -g ] [ -k ] [ -s ]" 1>&2
+  echo " -k : install KDE packages"
+  echo " -g : install Gnome packages"
+  echo " -s : install System76 drivers"
+}
+
+exit_abnormal() {
+  usage
+  exit 1
+}
+
+while getopts ":gk" options; do
+  case "${options}" in
+    g)
+      GNOME=1
+      ;;
+    k)
+      KDE=1
+      ;;
+    s)
+      SYSTEM76=1
+      ;;
+    :)
+      echo "Error: -${OPTARG} requires an argument."
+      exit_abnormal
+      ;;
+    *)
+      exit_abnormal
+      ;;
+  esac
+done
+
 set -e
 set -x
 
@@ -25,13 +62,13 @@ sudo add-apt-repository -y universe multiverse
 ## Chrome
 [ ! -f ~/Downloads/google-chrome-stable_current_amd64.deb ] && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/google-chrome-stable_current_amd64.deb
 
+## Ticktick
+[ ! -f ~/Downloads/ticktick-1.0.40-amd64.deb ] && wget https://appest-public.s3.amazonaws.com/download/linux/linux_deb_x64/ticktick-1.0.40-amd64.deb -O ~/Downloads/ticktick-1.0.40-amd64.deb
+
 # Add apt repositories
 ## Albert
 [ ! -f /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg ] && wget -qO - https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-
-## Ticktick
-[ ! -f ~/Downloads/ticktick-1.0.40-amd64.deb ] && wget https://appest-public.s3.amazonaws.com/download/linux/linux_deb_x64/ticktick-1.0.40-amd64.deb -O ~/Downloads/ticktick-1.0.40-amd64.deb
 
 ## Yubikey software
 sudo add-apt-repository -y ppa:yubico/stable
@@ -53,8 +90,19 @@ sudo apt-add-repository -y ppa:system76-dev/stable
 # Update package index files
 sudo apt update
 
-# Install apt packages
-sudo apt install -y albert bat build-essential flatpak fprintd gir1.2-gda-5.0 gir1.2-gsound-1.0 gir1.2-gtop-2.0 gnome-keyring gnome-shell-extension-manager gnome-tweaks gnuplot graphviz htop input-remapper libaio1 libdebconfclient0 libdevmapper-event1.02.1 libfido2-1 libfuse2 libgtop2-dev login lvm2 mokutil myrepos ncdu pcscd podman python3-pip silversearcher-ag sshuttle stow texlive-full tig tmux vim virt-manager virt-viewer virtinst wl-clipboard yubikey-manager yubikey-personalization zsh-autosuggestions zsh-syntax-highlighting zsh system76-driver scdaemon curl yubikey-manager libpam-yubico libpam-u2f btop
+APT_PKGS="albert bat build-essential flatpak fprintd gnome-keyring gnuplot graphviz htop input-remapper libfuse2 myrepos ncdu pcscd podman python3-pip silversearcher-ag sshuttle stow texlive-full tig tmux vim virt-manager virt-viewer virtinst yubikey-manager yubikey-personalization zsh-autosuggestions zsh-syntax-highlighting zsh system76-wallpapers scdaemon curl yubikey-manager libpam-yubico libpam-u2f btop"
+
+# Install core apt packages
+if [[ ${GNOME} == 1 ]]; then
+  APT_PKGS+=" gir1.2-gda-5.0 gir1.2-gsound-1.0 gir1.2-gtop-2.0 gnome-shell-extension-manager gnome-tweaks wl-clipboard"
+fi
+
+if [[ ${KDE} == 1 ]]; then
+  APT_PKGS+=""
+fi
+
+if [[ ${SYSTEM76} == 1 ]]; then
+  APT_PKGS+=" system76-drivers"
 
 # Install snaps
 sudo snap install authy bitwarden icloud-for-linux mattermost-desktop multipass slack spotify telegram-desktop zotero-snap morgen mailspring code
@@ -92,13 +140,17 @@ sudo udevadm control -R
 
 # Useful commands to run depending on the desktop
 echo "Need to run Stow to setup symlinks"
-echo "To turn off Evolution alarm pop-ups: gsettings set org.gnome.evolution-data-server.calendar notify-with-tray true"
 echo "To set Juntion as the default browser: xdg-settings set default-web-browser re.sonny.Junction.desktop"
 echo "To ensure that the Chrome profile options are in the menu: update-desktop-database ~/.local/share/applications"
-echo "Center windows in Gnome: gsettings set org.gnome.mutter center-new-windows true"
 echo "To have Junction find Chrome profiles: update-desktop-database ~/.local/share/flatpak/exports/share/applications"
-echo "Gnome Shell Extensions to install: Extension Sync"
 
+if [[ ${GNOME} == 1 ]]; then
+  echo "To turn off Evolution alarm pop-ups: gsettings set org.gnome.evolution-data-server.calendar notify-with-tray true"
+  echo "Center windows in Gnome: gsettings set org.gnome.mutter center-new-windows true"
+  echo "Gnome Shell Extensions to install: Extension Sync"
+fi
+
+echo "Run the below snippet for setting up YubiKeys"
 echo << EOF
 sudo apt install libpam-u2f
 pamu2fcfg | sudo tee -a /etc/u2f_mappings
