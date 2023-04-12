b620bff48bf3262bf8b0818eef11e7018d684b49
Tom Moyer <tom.moyer@uncc.edu>
2023-04-12 14:14:55 -0400

    Fixing a few things

--- roles/user-config/files/finish-yubikey-setup.sh
+++ roles/user-config/files/finish-yubikey-setup.sh
@@ -5,7 +5,12 @@ echo ""
 pamu2fcfg | tee u2f_mappings               # Main YubiKey
 
 echo ""
-read -p "Remove primary Yubikey insert backup Yubikey and then press any key" -n1 -s
+read -p "Remove primary Yubikey, insert backup Yubikey #1, and then press any key" -n1 -s
+echo ""
+pamu2fcfg -n | tee -a u2f_mappings   # Backup YubiKey
+
+echo ""
+read -p "Remove backup Yubikey #1, insert backup Yubikey #2, and then press any key" -n1 -s
 echo ""
 pamu2fcfg -n | tee -a u2f_mappings   # Backup YubiKey
 
@@ -13,7 +18,7 @@ echo >> u2f_mappings
 
 sudo mv u2f_mappings /etc
 echo ""
-read -p "Remove backup Yubikey insert primary Yubikey and then press any key" -n1 -s
+read -p "Remove backup Yubikey #2, insert primary Yubikey, and then press any key" -n1 -s
 echo ""
 
 gpg-connect-agent "scd serialno" "learn --force" /bye
