8bb2f2986510c0de6dce471b05b162a778b5f3b1
Tom Moyer <tom.moyer@uncc.edu>
2023-11-10 02:24:13 -0500

    more

--- refresh.sh
+++ refresh.sh
@@ -5,6 +5,16 @@ die() {
     exit 1
 }
 
+echo "Please choose system type:"
+echo "1 - Command-line only system"
+echo "2 - Desktop system"
+
+read -ep 'Select type: ' number
+[[ $number =~ ^[[:digit:]]+$ ]] ||
+    die '*** Error: you should have entered a number'
+(( ( (number=(10#$number)) <= 2 ) && number >= 0 )) ||
+    die '*** Error, number not in range 1..2'
+
 case $number in
 
   1)
