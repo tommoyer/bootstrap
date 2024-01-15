b292210ee63b9f79711d87bb08f68b3dd26e5c01
Tom Moyer <tom.moyer@uncc.edu>
2024-01-15 14:02:24 -0800

    more

--- roles/user-config/files/finish-setup.sh
+++ roles/user-config/files/finish-setup.sh
@@ -40,7 +40,7 @@ mkdir -p ~/Applications
 gh release download -R obsidianmd/obsidian-releases -p "Obsidian-$(gh release list -L 1 -R obsidianmd/obsidian-releases | awk '{print $1}').AppImage" -D ~/Applications
 gh release download -R probonopd/go-appimage -p 'appimaged-*x86_64.AppImage' continuous -D ~/Applications
 gh release download -R MuhammedKalkan/OpenLens -p "OpenLens-$(gh release list -R MuhammedKalkan/OpenLens -L 1 | awk '{print $1}' | sed 's/^v//').x86_64.AppImage" -D ~/Applications
-gh release download -R getstation/desktop-app -p Station-x86_64.AppImage -D ~/Applications
+gh release download -R AppImageCommunity/AppImageUpdate -p 'AppImageUpdate-x86_64.AppImage' continuous -D ~/Applications
 chmod +x ~/Applications/*.AppImage
 /home/tmoyer/Applications/appimaged*
 
